9a7f5ea8d02741fbc7ba7b0fc7d5c3cbe18a8b9d
RamiyaSiva <65622782+RamiyaSiva@users.noreply.github.com>
2021-12-02 20:26:45 +0530

    Update gtf_to_dictionary1.py

--- gtf_to_dictionary1.py
+++ gtf_to_dictionary1.py
@@ -5,24 +5,22 @@ import pandas as pd
 pd.options.mode.chained_assignment = None
 
 #read gtf file and add a header
-gtf = pd.read_csv('/Users/ramiyasivakumar/AMP/annotations/gencode.v29.primary_assembly.annotation.gtf.back', sep ="\t",header = None, names = ["Chr", "Group", "Type", "Start", "End", "Space", "Strand", "Space1", "Meta"])
-gtf
+gtf = pd.read_csv('/Users/ramiyasivakumar/AMP/annotations/gencode.v29.primary_assembly.annotation.gtf.bk', sep ="\t",header = None, names = ["Chr", "Group", "Type", "Start", "End", "Space", "Strand", "Space1", "Meta"])
 
 #filter gtf file for exon,UTR regions 
 region = ["exon", "UTR"]
 gtf_regions = gtf[gtf['Type'].isin(region)]
 
-#Drop the meta column, this will be added back later on
+#Drop the meta column (this will be added back later on)
 gtf_regions = gtf_regions.drop(['Meta'], axis=1)
 
 #reset index 
 gtf_regions.reset_index(inplace = True)
-gtf_regions
 
 #formatting the information present in the final column(splitting by ';' as a delimiter, dropping columns that aren't present in all rows, adding header)
 gtf_regions_meta = gtf[gtf['Type'].isin(region)]
 gtf_regions_expanded = pd.DataFrame(gtf_regions_meta.Meta.str.split(';').tolist())
-gtf_regions_expanded.drop(gtf_regions_expanded.columns[[9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]], axis=1, inplace=True)
+gtf_regions_expanded.drop(gtf_regions_expanded.columns[[9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22]], axis=1, inplace=True)
 gtf_regions_expanded.columns =['gene_id', 'transcript_id', 'gene_type', 'gene_name', 'transcript_type', 'transcript_name', 'exon_number', 'exon_id', 'level']
 gtf_regions_expanded
 
@@ -43,25 +41,7 @@ result['exon_number'] = result['exon_number'].str.replace(r'exon_number', '')
 result['exon_id'] = result['exon_id'].str.replace(r'exon_id', '')
 result['level'] = result['level'].str.replace(r'level', '')
 
-
-#create a unique id for each row 
-## unsure of which columns to use for this 
-## approach - run group_by on each column, identify which columns have unique values
-
-#is each column unique in some way --> yes 
-dup = result.duplicated()
-dup
-
-#create unique id - index 
-#result.duplicated(subset=['brand'])
-#cols = ['gene_name', 'exon_id']
-#result['UID'] = result[cols].apply(lambda row: '_'.join(row.values.astype(str)), axis=1)
-
-
-result
-
 #subset dataframe into 2 dataframes based on strand 
-
 #positive strand 
 gtf_pos = result[result['Strand'] == '+']
 #rename start and end to donor and acceptor
@@ -70,6 +50,25 @@ gtf_pos.rename(columns = {'Start':'Acceptor'}, inplace = True)
 
 gtf_pos = gtf_pos[['Chr','Type', 'Acceptor', 'Donor','exon_id', 'gene_name', 'transcript_id']]
 
+gtf_pos_plus = gtf_pos
+gtf_pos_minus = gtf_pos
+
+gtf_pos_plus = gtf_pos_plus.astype({"Acceptor":'int64', "Donor":'int64'})
+gtf_pos_minus = gtf_pos_minus.astype({"Acceptor":'int64', "Donor":'int64'})
+
+gtf_pos_plus['Acceptor'] = gtf_pos_plus['Acceptor'] + 1
+gtf_pos_plus['Donor'] = gtf_pos_plus['Donor'] + 1
+
+gtf_pos_minus['Acceptor'] = gtf_pos_minus['Acceptor'] - 1
+gtf_pos_minus['Donor'] = gtf_pos_minus['Donor'] - 1
+
+
+#combine all three dataframes into one 
+gtf_pos = gtf_pos.append([gtf_pos_minus, gtf_pos_plus])
+
+#sort
+gtf_pos = gtf_pos.astype({"Acceptor":'int64', "Donor":'int64'})
+gtf_pos.sort_values(by=['Acceptor'])
 gtf_pos = gtf_pos.astype(str)
 
 #add new columns suitable for dictionary creation
@@ -81,18 +80,32 @@ gtf_pos["ID"] = gtf_pos["exon_id"] +'_'+ gtf_pos["gene_name"]+'_'+ gtf_pos["tran
 
 gtf_pos.columns = gtf_pos.columns.str.replace(' ', '')
 
-gtf_pos
-
-#negative strand
-gtf_neg = result[result['Strand'] == '-']
-
-#run again with previous script 
-#rename donor and acceptor
+gtf_neg = result[result['Strand'] == '+']
+#rename start and end to donor and acceptor
 gtf_neg.rename(columns = {'Start':'Donor'}, inplace = True)
 gtf_neg.rename(columns = {'End':'Acceptor'}, inplace = True)
 
-#subsetting columns that will be kept in dictionary and reordering to match gtf_pos
 gtf_neg = gtf_neg[['Chr','Type', 'Acceptor', 'Donor','exon_id', 'gene_name', 'transcript_id']]
+
+gtf_neg_plus = gtf_neg
+gtf_neg_minus = gtf_neg
+
+gtf_neg_plus = gtf_neg_plus.astype({"Acceptor":'int64', "Donor":'int64'})
+gtf_neg_minus = gtf_neg_minus.astype({"Acceptor":'int64', "Donor":'int64'})
+
+gtf_neg_plus['Acceptor'] = gtf_neg_plus['Acceptor'] + 1
+gtf_neg_plus['Donor'] = gtf_neg_plus['Donor'] + 1
+
+gtf_neg_minus['Acceptor'] = gtf_neg_minus['Acceptor'] - 1
+gtf_neg_minus['Donor'] = gtf_neg_minus['Donor'] - 1
+
+
+#combine all three dataframes into one 
+gtf_neg = gtf_neg.append([gtf_neg_minus, gtf_neg_plus])
+
+#sort
+gtf_neg = gtf_neg.astype({"Acceptor":'int64', "Donor":'int64'})
+gtf_neg.sort_values(by=['Acceptor'])
 
 gtf_neg = gtf_neg.astype(str)
 
@@ -101,20 +114,20 @@ gtf_neg["acceptor_pos"] = gtf_neg["Chr"] +'_'+ gtf_neg["Acceptor"]
 gtf_neg["donor_pos"] = gtf_neg["Chr"] +'_'+ gtf_neg["Donor"]
 
 #UID
-gtf_neg["ID"] = gtf_neg["exon_id"]+'_'+gtf_neg["gene_name"]+'_'+gtf_neg["transcript_id"]
+gtf_neg["ID"] = gtf_neg["exon_id"] +'_'+ gtf_neg["gene_name"]+'_'+ gtf_neg["transcript_id"]
+
 gtf_neg.columns = gtf_neg.columns.str.replace(' ', '')
 
-gtf_neg
+
+gtf_neg = gtf_neg.replace({'\s': ''}, regex=True)
+gtf_pos = gtf_pos.replace({'\s': ''}, regex=True)
 
 #combine dataframes pos and neg
 frames = [gtf_pos, gtf_neg]
 gtf_dictionary = pd.concat(frames)
-gtf_dictionary
 
 
 donor_dictionary = gtf_dictionary[["ID", "donor_pos"]]
 acceptor_dictionary = gtf_dictionary[["ID", "acceptor_pos"]]
 donor_dictionary.to_csv('/Users/ramiyasivakumar/AMP/donor_dictionary.csv', sep='\t', header=True, index=False)
 acceptor_dictionary.to_csv('/Users/ramiyasivakumar/AMP/acceptor_dictionary.csv', sep='\t', header=True, index=False)
-
-
